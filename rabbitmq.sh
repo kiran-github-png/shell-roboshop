da12a7581ab68892e3708c6c2eb020a5b9fc90a9
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-25 17:23:12 +0530

    rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -11,7 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
-SCRIPT_DIR=$(PWD)
+SCRIPT_DIR=$PWD
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
